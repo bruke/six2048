a599c5e2b3586dfd1f3002a9cb76f21428da974e
bruke <boxiaohu@gmail.com>
2018-06-21 09:36:31 +0800

    start record

---
+++ assets/script/layers/StartRecordLayer.js
@@ -0,0 +1,26 @@
+
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+
+    onBtnRestart (evt) {
+
+    },
+
+    onBtnContinue (evt) {
+
+    },
+});
