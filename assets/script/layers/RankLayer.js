14ff9cc523acee0852f21694a7aad4a31dcaedcb
bruke <boxiaohu@gmail.com>
2018-06-22 09:14:31 +0800

    more ui layers

---
+++ assets/script/layers/RankLayer.js
@@ -0,0 +1,30 @@
+
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+
+    onBtnShare (evt) {
+
+    },
+
+    onBtnMoreGame (evt) {
+
+    },
+
+    onBtnClose (evt) {
+
+    },
+});
---
+++ assets/script/layers/ReviveLayer.js
@@ -0,0 +1,26 @@
+
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+
+    onBtnRevive (evt) {
+
+    },
+
+    onBtnClose (evt) {
+
+    },
+});
